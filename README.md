5fe3f536652ff15d74c1d4df2c1fbde1eaf1a4da
Gary Monson <gary.monson@gmail.com>
2015-09-26 19:23:19 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Falcon-autoCRUD
+
+## Quick start
+
+    virtualenv -p `which python3.4` virtualenv
+    source virtualenv/bin/activate
+    pip install -r requirements.txt
+    pip install -r dev_requirements.txt
+    nosetests
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(name='falcon-autocrud',
+      version='0.0.1',
+      description='Makes RESTful CRUD easier',
+      long_description=long_description,
+      url='https://bitbucket.org/garymonson/falcon-autocrud',
+      author='Gary Monson',
+      author_email='gary.monson@gmail.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3',
+      ],
+      keywords='falcon crud rest database',
+      packages=['falcon_autocrud'],
+      install_requires=[
+      ],
+      zip_safe=False)
