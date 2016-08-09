de2006c9c9ddac751f671c54bb42e7c14a46daea
Gary Monson <gary.monson@gmail.com>
2016-08-09 19:30:15 +1000

    Move fixtures into fixture file

--- falcon_autocrud/test.py
+++ falcon_autocrud/test.py
@@ -1,32 +1,13 @@
 from datetime import datetime, timedelta
 import json
 from sqlalchemy import create_engine, Column, DateTime, ForeignKey, Integer, Numeric, String, Time
-from sqlalchemy.orm import relationship
 
 from .resource import CollectionResource, SingleResource
 
 from .test_base import Base, BaseTestCase
 
+from .test_fixtures import Company, Employee
 
-class Company(Base):
-    __tablename__ = 'companies'
-    id          = Column(Integer, primary_key=True)
-    name        = Column(String(50), unique=True)
-    employees   = relationship('Employee')
-
-class Employee(Base):
-    __tablename__ = 'employees'
-    id          = Column(Integer, primary_key=True)
-    name        = Column(String(50), unique=True)
-    joined      = Column(DateTime())
-    left        = Column(DateTime(), nullable=True)
-    company_id  = Column(Integer, ForeignKey('companies.id'), nullable=True)
-    company     = relationship('Company', back_populates='employees')
-    pay_rate    = Column(Numeric(scale=4), nullable=True)
-    start_time  = Column(Time, nullable=True)
-    lunch_start = Column(Time, nullable=True)
-    end_time    = Column(Time, nullable=True)
-    caps_name   = Column(String(50))
 
 class CompanyCollectionResource(CollectionResource):
     model = Company
--- falcon_autocrud/test_fixtures.py
+++ falcon_autocrud/test_fixtures.py
@@ -1,5 +1,6 @@
 from .test_base import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Numeric, Time
+from sqlalchemy.orm import relationship
 
 
 class Account(Base):
@@ -7,3 +8,23 @@ class Account(Base):
     id          = Column(Integer, primary_key=True)
     name        = Column(String(50), unique=True)
     owner       = Column(String(50), unique=True)
+
+class Company(Base):
+    __tablename__ = 'companies'
+    id          = Column(Integer, primary_key=True)
+    name        = Column(String(50), unique=True)
+    employees   = relationship('Employee')
+
+class Employee(Base):
+    __tablename__ = 'employees'
+    id          = Column(Integer, primary_key=True)
+    name        = Column(String(50), unique=True)
+    joined      = Column(DateTime())
+    left        = Column(DateTime(), nullable=True)
+    company_id  = Column(Integer, ForeignKey('companies.id'), nullable=True)
+    company     = relationship('Company', back_populates='employees')
+    pay_rate    = Column(Numeric(scale=4), nullable=True)
+    start_time  = Column(Time, nullable=True)
+    lunch_start = Column(Time, nullable=True)
+    end_time    = Column(Time, nullable=True)
+    caps_name   = Column(String(50))
